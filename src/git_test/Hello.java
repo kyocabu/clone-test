e1afe9ae66639a112b7bcbca039b98447bd36f96
Takeshi Kyogoku <3711700+kyocabu@users.noreply.github.com>
2023-04-18 13:28:52 +0900

    表示文章を変更

--- src/git_test/Hello.java
+++ src/git_test/Hello.java
@@ -3,7 +3,7 @@ package git_test;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("HELLO!!!Git!!!");
+        System.out.println("HELLO!!!GitHub!!!");
     }
 
 }
